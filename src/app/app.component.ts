8767848f26445dc71b846f5382628c6960c53154
Louis Sutton <louis.sutton.iii@gmail.com>
2020-02-28 18:24:15 -0800

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +8,9 @@ import { from } from 'rxjs';
 })
 export class AppComponent {
 constructor(public _http: HttpClient) {}
+value;
 submit() {
-  this._http.post(`https://figuektqni.execute-api.us-west-1.amazonaws.com')
+  this._http.post(`https://figuektqni.execute-api.us-west-1.amazonaws.com`, { "title": this.value })
+
 }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
